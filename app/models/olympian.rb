7d96b988c288e9c696ea16085a668a6a663ef09c
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-30 12:23:06 -0600

    Add olympians method

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,2 +1,9 @@
 class Olympian < ApplicationRecord
+
+  def self.olympians
+    list = self.select(:name, :age, :team, :sport).distinct.map do |olymp|
+      Olympians.new(olymp)
+    end
+    return list
+  end
 end
