261717ceb84c7f6a1b7fef2217fc08b184bf75d8
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-30 16:21:05 -0600

    Add Olympian_stats PORO

---
+++ app/models/olympian_stats.rb
@@ -0,0 +1,21 @@
+class OlympianStats
+  attr_reader :id
+  def initialize
+    @id = 1
+  end
+  def total_competing_olympians
+    Olympian.select(:name).distinct.count
+  end
+
+  def average_weight
+    {
+      unit: 'kg',
+      male_olympians:  Olympian.where(sex: 'M').average(:weight).to_f.round(1),
+      female_olympians: Olympian.where(sex: 'F').average(:weight).to_f.round(1)
+    }
+  end
+
+  def average_age
+    Olympian.average(:age).to_f.round(1)
+  end
+end
