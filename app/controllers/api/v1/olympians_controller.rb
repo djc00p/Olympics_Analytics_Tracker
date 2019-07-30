8972cb5f636259c1b271993d851e09081ec6250e
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-30 13:42:39 -0600

    Add Sort by Youngest to Olympian Controller

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,8 +1,17 @@
 class  Api::V1::OlympiansController < ApplicationController
   def index
-  #   olympians = Olympian.all
-  #   serialized = OlympiansSerializer.new(olympians)
-    render json: { olympians: Olympian.olympians }
-    #
+    case params['age']
+    when 'youngest'
+      render json: Olympian.youngest
+    else
+      render json: { olympians: Olympian.olympians }
+    end
+  end
+
+  private
+
+  def olympian_params
+
+    params.require(:olympians).permit(:age)
   end
 end
