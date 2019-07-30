e04764d28f1b13efac723c5c3a71aaea20550fa3
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-30 16:18:43 -0600

    Add Serializer for Olympian stats

---
+++ app/serializers/olympian_stats_serializer.rb
@@ -0,0 +1,6 @@
+class OlympianStatsSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :total_competing_olympians,
+             :average_weight,
+             :average_age
+end
--- app/serializers/olympians_serializer.rb
+++
@@ -1,8 +0,0 @@
-class OlympiansSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :name,
-             :team,
-             :age,
-             :sport,
-             :total_medals_won
-end
